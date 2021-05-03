964b1429aff9ec712accb97c3f6ca99bfdb10e0b
ich777 <ich777@noreply.example.org>
2021-05-03 20:25:03 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "---Sending Gotify message to server address: '${GOTIFY_URL}' with title: '${PUSHOVER_TITLE}' and message: '${PUSHOVER_MESSAGE}'---"
+echo "---Sending Gotify message to server address: '${GOTIFY_URL}' with title: '${GOTIFY_TITLE}' and message: '${GOTIFY_MESSAGE}'---"
 wget -q "${GOTIFY_URL}message?token=${GOTIFY_TOKEN}" --post-data "title=${GOTIFY_TITLE}&message=${GOTIFY_MESSAGE}&priority=${GOTIFY_PRIORITY}"
 echo "--- D O N E ---"

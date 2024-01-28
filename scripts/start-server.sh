f684ff1b6a2fbf7df9f03c662498a7b9d6ace975
ich777 <christophhummer@gmail.com>
2024-01-28 21:44:31 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Replace HOST_IP with the IP from the Host - only works when container is ran on host interface!
+if grep -q "HOST_IP" <<< "${GOTIFY_MESSAGE}" ; then
+  HOST_IP=$(hostname -I | awk '{print $1}')
+  GOTIFY_MESSAGE=$(sed "s/HOST_IP/${HOST_IP}/" <<< "${GOTIFY_MESSAGE}")
+fi
+
 echo "---Sending Gotify message to server address: '${GOTIFY_URL}' with title: '${GOTIFY_TITLE}' and message: '${GOTIFY_MESSAGE}'---"
 wget -qO- "${GOTIFY_URL}message?token=${GOTIFY_TOKEN}" --post-data "title=${GOTIFY_TITLE}&message=${GOTIFY_MESSAGE}&priority=${GOTIFY_PRIORITY}" &>/dev/null
 echo "--- D O N E ---"
